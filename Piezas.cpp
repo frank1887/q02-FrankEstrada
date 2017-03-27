13502c5322e3a4404747ed385a29ea8702397f37
student <student@softeng>
2017-03-27 14:55:01 -0700

    completed q02

--- Piezas.cpp
+++ Piezas.cpp
@@ -13,21 +13,35 @@
  * [0,0][0,1][0,2][0,3]
  * So that a piece dropped in column 2 should take [0,2] and the next one
  * dropped in column 2 should take [1,2].
-**/
+ **/
 
 
 /**
  * Constructor sets an empty board (default 3 rows, 4 columns) and 
  * specifies it is X's turn first
-**/
-Piezas::Piezas();
+ **/
+Piezas::Piezas()
+{
+	//std::vector < std::vector<Piece> > board;
+	turn = X;
+	for(int i = 0; i < BOARD_ROWS; i++) {
+		std::vector <Piece> boardRow(BOARD_COLS);
+		board.push_back(boardRow);
+	}
+	reset();
+}
 
 /**
  * Resets each board location to the Blank Piece value, with a board of the
  * same size as previously specified
-**/
+ **/
 void Piezas::reset()
 {
+	for(int i = 0; i < BOARD_ROWS; i++) {
+		for(int j = 0; j < BOARD_COLS; j++) {
+			board[i][j] = Blank;
+		}
+	}
 }
 
 /**
@@ -37,14 +51,38 @@ void Piezas::reset()
  * In that case, placePiece returns Piece Blank value 
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
-Piece Piezas::dropPiece(int column);
+ **/ 
+Piece Piezas::dropPiece(int column)
+{
+	if(column < 0 || column >= BOARD_COLS) {
+		return Invalid;
+	}
+	for(int i = 0; i < BOARD_ROWS; i++) {
+		if(board[i][column] == Blank) {
+			board[i][column] = turn;
+			if(turn == X) 
+				turn = O;
+			else
+				turn = X;
+			return board[i][column];
+		}
+	}
+	return Blank;
+}
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
-Piece Piezas::pieceAt(int row, int column);
+ **/
+
+Piece Piezas::pieceAt(int row, int column)
+{
+	if(row < 0 || column < 0 || row >= BOARD_ROWS || column >= BOARD_COLS) {
+		return Invalid;
+	}
+	else
+		return board[row][column];
+}
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
@@ -54,5 +92,87 @@ Piece Piezas::pieceAt(int row, int column);
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
  * line, it is a tie.
-**/
-Piece Piezas::gameState();
+ **/
+Piece Piezas::gameState()
+{
+	for(int i = 0; i < BOARD_ROWS; i++) {
+		for(int j = 0; j < BOARD_COLS; j++) {
+			if(board[i][j] == Blank) {
+				return Invalid;
+			}
+		}
+	}
+	int xStreak = 1;
+	int oStreak = 1;
+	int xCurrentStreak = 1;
+	int oCurrentStreak = 1;
+
+	//horizontal
+	for(int i = 0; i < BOARD_ROWS; i++) {
+		for(int j = 0; j < BOARD_COLS - 1; j++) {
+			if(board[i][j] == X && board[i][j+1] == X)
+			{
+				xCurrentStreak++;
+				if(xCurrentStreak > xStreak) {
+					xStreak = xCurrentStreak;
+				}
+			}
+			else {
+				xCurrentStreak = 1;
+			}
+			if(board[i][j] == O && board[i][j+1] == O)
+			{
+				oCurrentStreak++;
+				if(oCurrentStreak > oStreak) {
+					oStreak = oCurrentStreak;
+				}
+			}
+			else {
+				oCurrentStreak = 1;
+			}
+		}
+		//reset active streak for next row
+		xCurrentStreak = 1;
+		oCurrentStreak = 1;
+	}
+
+	//vertical
+	for(int i = 0; i < BOARD_COLS; i++) {
+		for(int j = 0; j < BOARD_ROWS - 1; j++) {
+			if(board[j][i] == X && board[j+1][i] == X)
+			{
+				xCurrentStreak++;
+				if(xCurrentStreak > xStreak) {
+					xStreak = xCurrentStreak;
+				}
+			}
+			else {
+				xCurrentStreak = 1;
+			}
+			if(board[j][i] == O && board[j+1][i] == O)
+			{
+				oCurrentStreak++;
+				if(oCurrentStreak > oStreak) {
+					oStreak = oCurrentStreak;
+				}
+			}
+			else {
+				oCurrentStreak = 1;
+			}
+		}
+		//reset active streak for next row
+		xCurrentStreak = 1;
+		oCurrentStreak = 1;
+	}
+
+
+	if(oStreak > xStreak) {
+		return O;
+	}
+	else if(xStreak > oStreak) {
+		return X;
+	}
+	else {
+		return Blank;
+	}
+}
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -1,10 +1,10 @@
 /**
  * Unit Tests for Piezas
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
 	protected:
@@ -17,4 +17,241 @@ class PiezasTest : public ::testing::Test
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(PiezasTest, firstTurnShouldBeX)
+{
+	Piezas test;
+	Piece starting = test.dropPiece(0);
+	ASSERT_EQ(starting, X);
+}
+
+TEST(PiezasTest, secondTurnShouldBeO)
+{
+	Piezas test;
+	Piece second = test.dropPiece(0);
+	second = test.dropPiece(0);
+	ASSERT_EQ(second, O);
+}
+
+TEST(PiezasTest, columnLessThan0)
+{
+	Piezas test;
+	Piece starting = test.dropPiece(-1);
+	ASSERT_EQ(starting, Invalid);
+}
+
+TEST(PiezasTest, columnTooBig)
+{
+	Piezas test;
+	Piece starting = test.dropPiece(4);
+	ASSERT_EQ(starting, Invalid);
+}
+
+TEST(PiezasTest, columnOverflow)
+{
+	Piezas test;
+	Piece starting = test.dropPiece(2);
+	starting = test.dropPiece(2);
+	starting = test.dropPiece(2);
+	starting = test.dropPiece(2);
+	ASSERT_EQ(starting, Blank);
+}
+
+TEST(PiezasTest, pieceAtRowTooSmall)
+{
+	Piezas test;
+	Piece bad = test.pieceAt(-1, 2);
+	ASSERT_EQ(bad, Invalid);
+}
+
+TEST(PiezasTest, pieceAtColTooSmall)
+{
+	Piezas test;
+	Piece bad = test.pieceAt(1, -2);
+	ASSERT_EQ(bad, Invalid);
+}
+
+TEST(PiezasTest, pieceAtRowTooBig)
+{
+	Piezas test;
+	Piece bad = test.pieceAt(3, 1);
+	ASSERT_EQ(bad, Invalid);
+}
+
+TEST(PiezasTest, pieceAtColTooBig)
+{
+	Piezas test;
+	Piece bad = test.pieceAt(1, 4);
+	ASSERT_EQ(bad, Invalid);
+}
+
+TEST(PiezasTest, pieceAtBothBad)
+{
+	Piezas test;
+	Piece bad = test.pieceAt(-1, 4);
+	ASSERT_EQ(bad, Invalid);
+}
+
+TEST(PiezasTest, validPieceAtX)
+{
+	Piezas test;
+	Piece shouldBeX = test.dropPiece(0);
+	shouldBeX = test.pieceAt(0, 0);
+	ASSERT_EQ(shouldBeX, X);
+}
+
+TEST(PiezasTest, validPieceAtO)
+{
+	Piezas test;
+	Piece shouldBeO = test.dropPiece(0);
+	shouldBeO = test.dropPiece(0);
+	shouldBeO = test.pieceAt(1, 0);
+	ASSERT_EQ(shouldBeO, O);
+}
+
+TEST(PiezasTest, validPiecePlaceX)
+{
+	Piezas test;
+	Piece shouldBeX = test.dropPiece(0);
+	shouldBeX = test.dropPiece(0);
+	shouldBeX = test.dropPiece(0);
+	shouldBeX = test.pieceAt(2, 0);
+	ASSERT_EQ(shouldBeX, X);
+}
+
+TEST(PiezasTest, validPiecePlaceO)
+{
+	Piezas test;
+	Piece shouldBeO = test.dropPiece(0);
+	shouldBeO = test.dropPiece(0);
+	shouldBeO = test.dropPiece(2);
+	shouldBeO = test.dropPiece(2);
+	shouldBeO = test.pieceAt(1, 2);
+	ASSERT_EQ(shouldBeO, O);
+}
+
+TEST(PiezasTest, gameStateInvalid)
+{
+	Piezas test;
+	Piece shouldBeInvalid = test.gameState();
+	ASSERT_EQ(shouldBeInvalid, Invalid);
+}
+
+TEST(PiezasTest, xWinsRows)
+{
+	Piezas test;
+	Piece shouldBeX = test.dropPiece(0);
+	shouldBeX = test.dropPiece(0);
+	shouldBeX = test.dropPiece(1);
+	shouldBeX = test.dropPiece(1);
+	shouldBeX = test.dropPiece(2);
+	shouldBeX = test.dropPiece(0);
+	shouldBeX = test.dropPiece(3);
+	shouldBeX = test.dropPiece(3);
+	shouldBeX = test.dropPiece(2);
+	shouldBeX = test.dropPiece(2);
+	shouldBeX = test.dropPiece(1);
+	shouldBeX = test.dropPiece(3);
+	shouldBeX = test.gameState();
+	ASSERT_EQ(shouldBeX, X);
+}
+
+TEST(PiezasTest, oWinsRows)
+{
+	Piezas test;
+	Piece shouldBeO = test.dropPiece(0);
+	shouldBeO = test.dropPiece(0);
+	shouldBeO = test.dropPiece(0);
+	shouldBeO = test.dropPiece(2);
+	shouldBeO = test.dropPiece(1);
+	shouldBeO = test.dropPiece(1);
+	shouldBeO = test.dropPiece(3);
+	shouldBeO = test.dropPiece(2);
+	shouldBeO = test.dropPiece(3);
+	shouldBeO = test.dropPiece(3);
+	shouldBeO = test.dropPiece(1);
+	shouldBeO = test.dropPiece(2);
+	shouldBeO = test.gameState();
+	ASSERT_EQ(shouldBeO, O);
+}
+
+TEST(PiezasTest, tieOnRows)
+{
+	Piezas test;
+	Piece tie = test.dropPiece(0);
+	tie = test.dropPiece(0);
+	tie = test.dropPiece(0);
+
+	tie = test.dropPiece(3);
+	tie = test.dropPiece(1);
+	tie = test.dropPiece(1);
+
+	tie = test.dropPiece(1);
+	tie = test.dropPiece(3);
+	tie = test.dropPiece(3);
+
+	tie = test.dropPiece(2);
+	tie = test.dropPiece(2);
+	tie = test.dropPiece(2);
+
+	tie = test.gameState();
+	ASSERT_EQ(tie, Blank);
+}
+
+TEST(PiezasTest, xWinsCols)
+{
+	Piezas test;
+	Piece shouldBeX = test.dropPiece(0);
+	shouldBeX = test.dropPiece(1);
+	shouldBeX = test.dropPiece(0);
+	shouldBeX = test.dropPiece(3);
+	shouldBeX = test.dropPiece(2);
+	shouldBeX = test.dropPiece(3);
+	shouldBeX = test.dropPiece(0);
+	shouldBeX = test.dropPiece(2);
+	shouldBeX = test.dropPiece(1);
+	shouldBeX = test.dropPiece(1);
+	shouldBeX = test.dropPiece(3);
+	shouldBeX = test.dropPiece(2);
+	shouldBeX = test.gameState();
+	ASSERT_EQ(shouldBeX, X);
+}
+
+TEST(PiezasTest, oWinsCols)
+{
+	Piezas test;
+	Piece shouldBeO = test.dropPiece(1);
+	shouldBeO = test.dropPiece(0);
+	shouldBeO = test.dropPiece(3);
+	shouldBeO = test.dropPiece(2);
+	shouldBeO = test.dropPiece(3);
+	shouldBeO = test.dropPiece(0);
+	shouldBeO = test.dropPiece(2);
+	shouldBeO = test.dropPiece(1);
+	shouldBeO = test.dropPiece(1);
+	shouldBeO = test.dropPiece(3);
+	shouldBeO = test.dropPiece(2);
+	shouldBeO = test.dropPiece(0);
+	shouldBeO = test.gameState();
+	ASSERT_EQ(shouldBeO, O);
+}
+
+TEST(PiezasTest, tieOnCols)
+{
+	Piezas test;
+	Piece tie = test.dropPiece(0);
+	tie = test.dropPiece(1);
+	tie = test.dropPiece(0);
+	tie = test.dropPiece(3);
+	tie = test.dropPiece(2);
+	tie = test.dropPiece(3);
+	tie = test.dropPiece(0);
+	tie = test.dropPiece(2);
+	tie = test.dropPiece(1);
+	tie = test.dropPiece(1);
+	tie = test.dropPiece(2);
+	tie = test.dropPiece(3);
+	tie = test.gameState();
+	ASSERT_EQ(tie, Blank);
 }
